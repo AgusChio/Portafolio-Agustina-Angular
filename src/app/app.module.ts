2d56d5b55b1ccebc1e7e01f5921cbccbd61d1b3c
Agustina Chiodini <agus.chio25790@gmail.com>
2023-04-13 22:20:11 -0300

    agregando todos los componentes(navbar, sobremi, servicios, certificaciones, proyectos, contacto, main), es responsive todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { CertificacionesComponent } from './certificaciones/certificaciones.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    SobreMiComponent,
+    ServiciosComponent,
+    CertificacionesComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/certificaciones/certificaciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificaciones',
+  templateUrl: './certificaciones.component.html',
+  styleUrls: ['./certificaciones.component.css']
+})
+export class CertificacionesComponent {
+
+}
