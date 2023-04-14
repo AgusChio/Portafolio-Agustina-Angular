eb025bd246958325ab77277f23bfc31c0c9a0b2d
Agustina Chiodini <agus.chio25790@gmail.com>
2023-04-14 02:18:30 -0300

    se agrego email.js con respuesta automatica , falta poner un sweet alert para confirmar el envio

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_g2o3jr7', 'template_r4xzopv', e.target as HTMLFormElement, 'Vnogd5lB5MolmHWc0')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 
 }
