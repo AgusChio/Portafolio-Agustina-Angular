6de33be770acfe3106e82d3e6f25c4a065f3e4e5
Agustina Chiodini <agus.chio25790@gmail.com>
2023-04-14 21:35:00 -0300

    agregue un sweetalert para el correcto envio y fallo del envio del mensaje de contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
+
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
@@ -12,9 +15,18 @@ export class ContactoComponent {
     e.preventDefault();
     emailjs.sendForm('service_g2o3jr7', 'template_r4xzopv', e.target as HTMLFormElement, 'Vnogd5lB5MolmHWc0')
       .then((result: EmailJSResponseStatus) => {
-        console.log(result.text);
+        Swal.fire({
+          text: '¡Correo enviado!',
+          icon: 'success'
+        }).then(() => {
+          window.location.reload();
+        });
       }, (error) => {
         console.log(error.text);
+        Swal.fire({
+          text: 'Por favor, verifique que haya completado todos los campos, de otra manera intente mas tarde. Muchas gracias!',
+          icon: 'error'
+        });
       });
   }
 
